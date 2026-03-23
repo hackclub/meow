1c8ae9e353d0ec01689dab78009e947d3102c644
Echo <github@3kh0.net>
2026-03-22 20:10:42 -0400

    vercel it up

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,12 +1,5 @@
 import type { PageServerLoad } from './$types';
-import { COUNTER_KEY } from '$env/static/private';
 
-export const load: PageServerLoad = async ({ fetch }) => {
-	await fetch('https://api.counterapi.dev/v2/alex-van-dorens-team-1946/first-counter-1946/up', {
-		headers: {
-			'Authorization': `Bearer ${COUNTER_KEY}`
-		}
-	});
-
+export const load: PageServerLoad = async () => {
 	return {};
 };
